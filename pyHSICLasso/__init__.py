04ef00b36e9aa52abcc91cafd67f26178f2581e6
Xiaowen Li <lxw243@gmail.com>
2023-05-24 04:24:06 +1200

    Update __init__.py

--- pyHSICLasso/__init__.py
+++ pyHSICLasso/__init__.py
@@ -6,7 +6,7 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 from future import standard_library
-from .api import HSICLasso
+from .api import MKHSICLasso
 from .hsic_lasso import hsic_lasso
 from .input_data import input_csv_file, input_tsv_file, input_matlab_file
 standard_library.install_aliases()
